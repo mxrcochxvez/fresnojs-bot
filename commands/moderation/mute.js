bbd21f335441c15d4ec65376a34b5d2e21d14750
Marco Chavez <marcode.chavez.jr@gmail.com>
2020-06-28 00:39:04 -0700

    setting up a mute command

---
+++ commands/moderation/mute.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "mute",
+    category: "moderation",
+    description: "Mutes annoying people",
+    run: async (client, message, args) => {
+        console.log();
+    }
+}
